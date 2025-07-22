0eab1005c0a99a517b4a56d7d38c63272d4a9451
Aksi147 <akjr820@gmail.com>
2025-07-22 12:56:00 -0400

    saved scratchpad.js created valid user input function

--- scratchpad.js
+++ scratchpad.js
@@ -1,37 +1,65 @@
-const testBoard1 = {
-  A: [
-    { type: "large", id: 1, hit: false }, // Represents position A0
-    { type: "small", id: 2, hit: true }, // Represents position A1
-    { type: "small", id: 2, hit: false }, // Represents position A2
-  ],
-  B: [
-    { type: "large", id: 1, hit: false }, // Represents position B0
-    { type: "empty", hit: false }, // Represents position B1
-    { type: "empty", hit: true }, // Represents position B2
-  ],
-  C: [
-    { type: "large", id: 1, hit: false }, // Represents position C0
-    { type: "empty", hit: false }, // Represents position C1
-    { type: "empty", hit: false }, // Represents position C2
-  ],
-};
+const readlineSync = require("readline-sync");
 
+// symbols for shot effect
 const hitLarge = "🔵";
 const hitSmall = "🟠";
 const miss = "❗";
 
-function printBoard(board) {
-  function revealShip(cell) {
-    if (cell.type === "large") return hitLarge;
-    if (cell.type === "small") return hitSmall;
-    else return "-";
+// actual board for game
+let board3 = [
+  [
+    { type: "large", id: 1, hit: false }, // Represents position A0
+    { type: "empty", hit: false }, // Represents position A1
+    { type: "empty", hit: false }, // Represents position A2
+  ],
+  [
+    { type: "large", hit: false }, // Represents position B0
+    { type: "empty", hit: false }, // Represents position B1
+    { type: "empty", hit: false }, // Represents position B2
+  ],
+  [
+    { type: "large", hit: false }, // Represents position C0
+    { type: "small", id: 1, hit: false }, // Represents position C1
+    { type: "small", hit: false }, // Represents position C2
+  ],
+];
+
+const rowMap = { A: 0, B: 1, C: 2 };
+
+// GAME STARTS HERE // GAME STARTS HERE // GAME STARTS HERE // GAME STARTS HERE // GAME STARTS HERE //
+
+gameOver = false;
+
+//prompt 1
+console.log("Welcome to Battleship!");
+
+// const wantToPlay = readlineSync.keyInYN("Would you like to play?");
+// if (!wantToPlay) {
+//   console.log("Fine! I didnt want to play either 🥲");
+//   process.exit(0);
+// }
+
+let userInput = readlineSync.question("Type in your coordinate!");
+
+let rowIndex = rowMap[userInput[0].toUpperCase()];
+let colIndex = userInput[1];
+
+function guessCoord(board) {
+  while (!userInput.match(/[A-C][0-2]/i)) {
+    userInput = readlineSync.question("Please type in a valid coordinate!");
+    rowIndex = rowMap[userInput[0].toUpperCase()];
+    colIndex = userInput[1];
   }
-  for (let [row] of Object.entries(board)) {
-    for (let col = 0; col < board[row].length; col++) {
-      board[row][col] = revealShip(board[row][col]);
+  if (userInput.match(/[A-C][0-2]/i)) {
+    console.log("the coordinate works");
+
+    function setHitTrue(board) {
+      for (userInput of board) {
+        board[rowIndex][colIndex].hit = true;
+      }
     }
+    setHitTrue(board);
   }
-  console.table(board);
 }
 
-printBoard(testBoard1);
+guessCoord(board3);
