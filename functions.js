0359d3ab73c5fe6cee079986e3b16f8eb966254f
Aksi147 <akjr820@gmail.com>
2025-07-24 18:38:40 -0400

    finished removing nested functions and removed redundant and repetitive code/functions

--- functions.js
+++ functions.js
@@ -6,33 +6,66 @@ const hitLarge = "🔵";
 const hitSmall = "🟠";
 const miss = "❗";
 
-const config = { boardSize: { 4: board4, 5: board5, 6: board6 } };
+const rowLabels = ["A", "B", "C", "D", "E", "F"];
+
+const config = {
+  boardSize: {
+    4: {
+      board: board4,
+      rowLabels: rowLabels.slice(0, 4),
+      labeledBoard: {
+        A: [],
+        B: [],
+        C: [],
+        D: [],
+      },
+      regex: /[A-D][0-3]/i,
+    },
+    5: {
+      board: board5,
+      rowLabels: rowLabels.slice(0, 5),
+      labeledBoard: {
+        A: [],
+        B: [],
+        C: [],
+        D: [],
+        E: [],
+      },
+      regex: /[A-E][0-4]/i,
+    },
+    6: {
+      board: board6,
+      rowLabels,
+      labeledBoard: {
+        A: [],
+        B: [],
+        C: [],
+        D: [],
+        E: [],
+        F: [],
+      },
+      regex: /[A-F][0-5]/i,
+    },
+  },
+};
+
+function ascii() {
+  console.log(
+    `
+  ========
+  __   _______ _   _   _    _ _____ _   _
+  \\ \\ / /  _  | | | | | |  | |_   _| \\ | |
+   \\ V /| | | | | | | | |  | | | | |  \\| |
+    \\ / | | | | | | | | |/\\| | | | | . ' |
+    | | \\ \\_/ / |_| | \\  /\\  /_| |_| |\\  |
+    \\_/  \\___/ \\___/   \\/  \\/ \\___/\\_| \\_/ 
+  ========
+`
+  );
+}
 
 const rowMap = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5 };
-const rowLabels = ["A", "B", "C", "D", "E", "F"];
-const rowLabels4 = rowLabels.slice(0, 4);
-const rowLabels5 = rowLabels.slice(0, 5);
-let labeledBoard4 = {
-  A: [],
-  B: [],
-  C: [],
-  D: [],
-};
-let labeledBoard5 = {
-  A: [],
-  B: [],
-  C: [],
-  D: [],
-  E: [],
-};
-let labeledBoard6 = {
-  A: [],
-  B: [],
-  C: [],
-  D: [],
-  E: [],
-  F: [],
-};
+
 function reveal(board, labeledBoard, rowLabels) {
   // assigns symbol to cell
   function getHitSymbol(board, row, col) {
@@ -53,122 +86,8 @@ function reveal(board, labeledBoard, rowLabels) {
   console.table(labeledBoard);
 }
 
-function ascii() {
-  console.log(
-    `
-  ========
-  __   _______ _   _   _    _ _____ _   _
-  \\ \\ / /  _  | | | | | |  | |_   _| \\ | |
-   \\ V /| | | | | | | | |  | | | | |  \\| |
-    \\ / | | | | | | | | |/\\| | | | | . ' |
-    | | \\ \\_/ / |_| | \\  /\\  /_| |_| |\\  |
-    \\_/  \\___/ \\___/   \\/  \\/ \\___/\\_| \\_/ 
-  ========
-`
-  );
-}
-
 function setHitTrue(board, row, col) {
   board[row][col].hit = true;
-}
-
-function guessCoord4x4(board) {
-  while (!allShipsDestroyed(board)) {
-    let userInput = readlineSync.question("Make a guess eg.. A1, B2, etc...");
-    console.clear();
-
-    let rowIndex = rowMap[userInput[0].toUpperCase()];
-    let colIndex = parseInt(userInput[1]);
-    while (!userInput.match(/[A-D][0-3]/i)) {
-      reveal(board, labeledBoard4, rowLabels4);
-      userInput = readlineSync.question(
-        "Make a valid guess eg.. A1, B2, etc..."
-      );
-      console.clear();
-      rowIndex = rowMap[userInput[0].toUpperCase()];
-      colIndex = userInput[1];
-    }
-
-    if (board[rowIndex][colIndex].type === "large") {
-      console.log("Great shot! You hit a large ship!");
-    }
-    if (board[rowIndex][colIndex].type === "small") {
-      console.log("Great shot! You hit a small ship!");
-    }
-    if (board[rowIndex][colIndex].type === "empty") {
-      console.log("Oh no! You missed!");
-    }
-
-    setHitTrue(board, rowIndex, colIndex);
-    reveal(board, labeledBoard4, rowLabels4);
-  }
-  ascii();
-}
-
-function guessCoord5x5(board) {
-  while (!allShipsDestroyed(board)) {
-    let userInput = readlineSync.question("Make a guess eg.. A1, B2, etc...");
-    console.clear();
-
-    let rowIndex = rowMap[userInput[0].toUpperCase()];
-    let colIndex = parseInt(userInput[1]);
-    while (!userInput.match(/[A-E][0-4]/i)) {
-      reveal(board, labeledBoard5, rowLabels5);
-      userInput = readlineSync.question(
-        "Make a valid guess eg.. A1, B2, etc..."
-      );
-      console.clear();
-      rowIndex = rowMap[userInput[0].toUpperCase()];
-      colIndex = userInput[1];
-    }
-
-    if (board[rowIndex][colIndex].type === "large") {
-      console.log("Great shot! You hit a large ship!");
-    }
-    if (board[rowIndex][colIndex].type === "small") {
-      console.log("Great shot! You hit a small ship!");
-    }
-    if (board[rowIndex][colIndex].type === "empty") {
-      console.log("Oh no! You missed!");
-    }
-
-    setHitTrue(board, rowIndex, colIndex);
-    reveal(board, labeledBoard5, rowLabels5);
-  }
-  ascii();
-}
-
-function guessCoord6x6(board) {
-  while (!allShipsDestroyed(board)) {
-    let userInput = readlineSync.question("Make a guess eg.. A1, B2, etc...");
-    console.clear();
-
-    let rowIndex = rowMap[userInput[0].toUpperCase()];
-    let colIndex = parseInt(userInput[1]);
-    while (!userInput.match(/[A-F][0-5]/i)) {
-      reveal(board, labeledBoard6, rowLabels);
-      userInput = readlineSync.question(
-        "Make a valid guess eg.. A1, B2, etc..."
-      );
-      console.clear();
-      rowIndex = rowMap[userInput[0].toUpperCase()];
-      colIndex = userInput[1];
-    }
-
-    if (board[rowIndex][colIndex].type === "large") {
-      console.log("Great shot! You hit a large ship!");
-    }
-    if (board[rowIndex][colIndex].type === "small") {
-      console.log("Great shot! You hit a small ship!");
-    }
-    if (board[rowIndex][colIndex].type === "empty") {
-      console.log("Oh no! You missed!");
-    }
-
-    setHitTrue(board, rowIndex, colIndex);
-    reveal(board, labeledBoard6, rowLabels);
-  }
-  ascii();
 }
 
 function allShipsDestroyed(board) {
@@ -183,8 +102,38 @@ function allShipsDestroyed(board) {
   return true;
 }
 
-module.exports = {
-  guessCoord4x4,
-  guessCoord5x5,
-  guessCoord6x6,
-};
+function guessCoord(size) {
+  const { board, rowLabels, labeledBoard, regex } = config.boardSize[size];
+  while (!allShipsDestroyed(board)) {
+    let userInput = readlineSync.question("Make a guess eg.. A1, B2, etc...");
+    console.clear();
+
+    let rowIndex = rowMap[userInput[0].toUpperCase()];
+    let colIndex = parseInt(userInput[1]);
+    while (!userInput.match(regex)) {
+      reveal(board, labeledBoard, rowLabels);
+      userInput = readlineSync.question(
+        "Make a valid guess eg.. A1, B2, etc..."
+      );
+      console.clear();
+      rowIndex = rowMap[userInput[0].toUpperCase()];
+      colIndex = userInput[1];
+    }
+
+    if (board[rowIndex][colIndex].type === "large") {
+      console.log("Great shot! You hit a large ship!");
+    }
+    if (board[rowIndex][colIndex].type === "small") {
+      console.log("Great shot! You hit a small ship!");
+    }
+    if (board[rowIndex][colIndex].type === "empty") {
+      console.log("Oh no! You missed!");
+    }
+
+    setHitTrue(board, rowIndex, colIndex);
+    reveal(board, labeledBoard, rowLabels);
+  }
+  ascii();
+}
+
+module.exports = { guessCoord };
--- runBattleship.js
+++ runBattleship.js
@@ -1,13 +1,6 @@
 const readlineSync = require("readline-sync");
-const {
-  board4,
-  board5,
-  board6,
-  board4x4,
-  board5x5,
-  board6x6,
-} = require("./boards");
-const { guessCoord4x4, guessCoord5x5, guessCoord6x6 } = require("./functions");
+const { board4x4, board5x5, board6x6 } = require("./boards");
+const { guessCoord } = require("./functions");
 
 //prompt 1
 console.log("Welcome to Battleship 🚢");
@@ -16,21 +9,17 @@ let boards = ["4x4", "5x5", "6x6"];
 let index = readlineSync.keyInSelect(boards, "Choose a Board Size");
 console.clear();
 
-let board;
 if (index === 0) {
-  board = board4;
   console.table(board4x4);
-  guessCoord4x4(board);
+  guessCoord(4);
 }
 if (index === 1) {
-  board = board5;
   console.table(board5x5);
-  guessCoord5x5(board);
+  guessCoord(5);
 }
 if (index === 2) {
-  board = board6;
   console.table(board6x6);
-  guessCoord6x6(board);
+  guessCoord(6);
 }
 
 process.exit(0);
